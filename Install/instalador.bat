e0d9e997547b6470e0e4e9d89764b210f68e13d1
Jonatha Cardoso <ojonathacardoso@gmail.com>
2019-09-08 20:32:04 -0300

    Instalador dos pacotes + Ajustes nos Presets

---
+++ Install/instalador.bat
@@ -0,0 +1,49 @@
+@ECHO OFF
+
+title instalador_tm
+
+wget.exe -O download\Facepager.exe https://github.com/strohne/Facepager/releases/download/v4.0.4/Facepager_Setup_4_0_4.windows.exe
+
+wget.exe -O download\OpenRefine.zip https://github.com/OpenRefine/OpenRefine/releases/download/3.2/openrefine-win-3.2.zip
+
+wget.exe -O download\SOBEK.jar http://sobek.ufrgs.br
+
+wget.exe -O download\R.exe  https://cran.r-project.org/bin/windows/base/R-3.6.1-win.exe
+
+wget.exe -O download\RStudio.exe https://download1.rstudio.org/desktop/windows/RStudio-1.2.1335.exe
+
+wget.exe -O download\RTools.exe  https://cran.r-project.org/bin/windows/Rtools/Rtools35.exe
+
+wget.exe -O download\RAnalyzer.zip https://github.com/ojonathacardoso/ranalyzer/archive/master.zip
+
+wget.exe -O download\FacepagerPresets.zip https://github.com/ojonathacardoso/ranalyzer-adds/archive/master.zip
+
+download\Java.exe /s
+
+download\Facepager.exe /S
+
+download\R.exe /SILENT
+
+download\RStudio.exe /S
+
+download\RTools.exe /SILENT
+
+unzip.exe download\OpenRefine.zip -d OpenRefine
+
+REM del download\OpenRefine.zip
+
+unzip.exe download\FacepagerPresets.zip -d FacepagerPresets
+
+copy download\FacepagerPresets\ranalyzer-adds-master\Facepager\Presets %USERPROFILE%\Facepager\Presets
+
+REM del download\FacepagerPresets.zip 
+
+unzip.exe download\RAnalyzer.zip -d RAnalyzer
+
+REM del download\RAnalyzer.zip 
+
+"C:\Program Files\R\R-3.6.1\bin\Rscript.exe" "download\RAnalyzer\ranalyzer-master\install.R"
+
+mkdir %USERPROFILE%\R\ranalyzer
+
+copy download\RAnalyzer\ranalyzer-master\ %USERPROFILE%\R\ranalyzer
